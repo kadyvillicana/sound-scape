6fc80fbe45e57b84add2c235171802a434a075fd
Carlos Villicana <carlosv@stokkur.is>
2023-10-05 21:55:26 -0600

    Added react-navigation, prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,39 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "react", "react-hooks", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    indent: ["error", "tab"],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
 };
